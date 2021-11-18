87429337d4e2ac85e446ee84e9152811346ebff0
Sanket Mhaiskar <sanketmhaiskar@gmail.com>
2021-11-18 09:47:11 -0800

    Delete Credentials.py

--- Credentials.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Sanket'
-
-#consumer key, consumer secret, access token, access secret.
-ckey="75VgfAdnk7svfShA8mQ1QkM5t"
-csecret="XRy96S7uOzuGmsRS4m7eON2wNXmFQm5ZWa29BUr2XaBIets00h"
-atoken="3607850245-lDiH2jfZlkF6kWDXfbXmudUWh3j2Dxyw3VmdtUL"
-asecret="TfU8lucwsJ1cmabbIla3UIPLcUsvEXK1GoSx72ydvIitE"
